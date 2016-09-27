96f89cad7a388921eb9e6fda5ff96ac030940911
Corinna Klaukin <corinna.klaukin@haw-hamburg.de>
2016-09-27 20:23:29 +0200

    make szene nicer with shadows and viewing angle

--- src/renderer.js
+++ src/renderer.js
@@ -1,47 +1,68 @@
 "use strict"
 
-let scene = new THREE.Scene()
-let camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 1000)
-scene.add(camera)
-camera.position.set(15, 10, 150)
-
-const directional_light = new THREE.DirectionalLight(0xffffff, 1)
-directional_light.position.set(1, 1, 1)
-scene.add(directional_light)
-const ambient_light = new THREE.AmbientLight(0x404040)
-scene.add(ambient_light)
-
 let renderer = new THREE.WebGLRenderer({
-    alpha: true
+    antialias: true
 })
-renderer.setClearColor(0xffffff, 0);
-renderer.autoClear = false
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+renderer.setClearColor(0xffffff, 0)
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+let scene = new THREE.Scene()
+let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000)
+camera.position.set(50, 100, 300)
+camera.lookAt({
+    x: 0,
+    y: -100,
+    z: 0
+})
+
+let ambient = new THREE.AmbientLight(0x444444)
+scene.add(ambient)
+
+let light = new THREE.DirectionalLight(0xaaaaaa, 2)
+light.position.set(1, -1, 1).normalize()
+scene.add(light)
+
+let spotLight = new THREE.SpotLight(0xFFAA88)
+spotLight.target.position.set(0, -100, 0)
+spotLight.position.set(20, 200, 20)
+spotLight.shadow.camera.near = 0.01
+spotLight.castShadow = true
+scene.add(spotLight)
 
 // make pretty background
 let loader = new THREE.TextureLoader()
 let texture = loader.load('assets/palmtrees.jpg')
 let backgroundMesh = new THREE.Mesh(
     new THREE.PlaneGeometry(1024, 1024),
-    new THREE.MeshBasicMaterial({
+    new THREE.MeshLambertMaterial({
+        color: 0x00ccff,
         map: texture
     }))
+backgroundMesh.receiveShadow = false
+backgroundMesh.castShadow = false
 backgroundMesh.material.depthTest = false
 backgroundMesh.material.depthWrite = false
 scene.add(backgroundMesh)
 
 // build the floor
-let plane_geometry = new THREE.PlaneGeometry(800, 800)
-let plane_material = new THREE.MeshBasicMaterial({
-    color: 0x006600,
-    side: THREE.DoubleSide,
-    map: texture
+let ground_geometry = new THREE.CubeGeometry(400, 0.2, 400)
+let ground_texture = loader.load('assets/palmtrees.jpg')
+let ground_material = new THREE.MeshPhongMaterial({
+    color: 0x00ff00,
+    shininess: 0,
+    specular: 0x888888,
+    shading: THREE.SmoothShading,
+    map: ground_texture
 })
-let plane = new THREE.Mesh(plane_geometry, plane_material)
-//scene.add(plane)
-//plane.rotation.x -= 90
-//plane.position.y -= 120
+var ground = new THREE.Mesh(ground_geometry, ground_material)
+ground.position.y = -101
+scene.add(ground)
+
+ground.castShadow = false
+ground.receiveShadow = true
 
 window.addEventListener('resize', function() {
     camera.aspect = window.innerWidth / window.innerHeight
@@ -49,35 +70,28 @@ window.addEventListener('resize', function() {
     renderer.setSize(window.innerWidth, window.innerHeight)
 }, false)
 
-let arrows = () => {
-    const axisHelper = new THREE.AxisHelper(100)
-    scene.add(axisHelper)
-}
-
-let octree = new THREE.Octree({
-    //scene: scene
-})
+let octree = new THREE.Octree()
 
 let balls = []
 let id = 0
 THREE.Mesh.prototype.velocity = new THREE.Vector3()
 THREE.Mesh.prototype.birthday = 0
-let geometry = new THREE.SphereGeometry(1, 32, 32)
+let boid_geometry = new THREE.SphereGeometry(1, 32, 32)
 
 let addBoidToSzene = (boid) => {
-    let mesh = new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({
-        //color: Math.random() * 0xffffff
-        color: 0xdc143c
+    let mesh = new THREE.Mesh(boid_geometry, new THREE.MeshPhongMaterial({
+        color: 0xdc143c,
+        shininess: 300,
+        specular: 0x33AA33,
+        shading: THREE.SmoothShading
     }))
-    //let mesh = new THREE.Mesh(flamingoGeometry, flamingoMaterial)
-    //let mixer = new THREE.AnimationMixer(mesh)
-    //mixer.clipAction(flamingoGeometry.animations[0]).setDuration(1).play()
-    //mixers.push(mixer)
+    mesh.castShadow = true
+    mesh.receiveShadow = false
     mesh.position.set(...boid.position.toArray())
     mesh.velocity = boid.velocity
     mesh.birthday = boid.boid.genotype.birthday
-        //mesh.scale.set(0.5, 0.5, 0.5)
-    //mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 5 + 2
+    let scale_factor = 3
+    mesh.scale.set(scale_factor, scale_factor, scale_factor)
     balls.push(mesh)
     scene.add(mesh)
     octree.add(mesh)
@@ -96,10 +110,6 @@ let removeBoidFromSzene = (boid) => {
 let renderWorld = () => {
     stats.begin()
     updateWorld()
-        //let delta = clock.getDelta();
-        //for (let i = 0; i < mixers.length; i++) {
-        //    mixers[i].update(delta);
-        //}
     renderer.clear()
     renderer.render(scene, camera)
     octree.update()
